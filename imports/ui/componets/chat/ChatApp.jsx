e8416b34be5de6c50d7cc8a057683389c39336aa
Ryota Saito <ryocsaito@gmail.com>
2016-11-11 14:49:14 -0800

    added form to submit messages

--- imports/ui/componets/chat/ChatApp.jsx
+++ imports/ui/componets/chat/ChatApp.jsx
@@ -1,18 +1,46 @@
 import React, { Component, PropTypes } from 'react'
-
+import MessageList from './MessageList.jsx'
 export default class ChatApp extends Component {
   constructor(props){
       super(props);
       this.state = {
       }
+
+      this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleSubmit(e){
+    e.preventDefault();
+    alert("Entered form");
+  }
+
 
   render(){
     return (
-      <h1> chat goes here</h1>
+      <div>
+        <MessageList />
+        <form id = "message-form" onSubmit = {this.handleSubmit}>
+          Enter Message here
+          <div className="form-group">
+            <input type="text"
+                  id="message-input"
+                  className="form-control input-lg"
+                  placeholder="Enter message here"/>
+          </div>
+
+          <div className="form-group text-center">
+            <input type="submit"
+                  id="submit-button"
+                  className="btn btn-primary btn-lg btn-block"
+                  value="Login" />
+          </div>
+        </form>
+      </div>
+
+
+    //need a form to submit messages
     );
   }
-
 }
 
 ChatApp.PropTypes = {
---
+++ imports/ui/componets/chat/MessageList.jsx
@@ -0,0 +1,23 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class MessageList extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+      }
+  }
+
+  render(){
+    return (
+      <h1> messages go here</h1>
+    );
+  }
+
+}
+
+// class Message extends Component{
+//
+// }
+
+MessageList.PropTypes = {
+}
